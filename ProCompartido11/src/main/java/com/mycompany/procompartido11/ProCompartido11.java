1d8b0faa5bbcb6f285a951fe426ff6b2207c0630
fernando <fernandoyucsan.chang.cam.alu@iesjulioverne.esgit>
2024-05-24 12:49:48 +0200

    version4

---
+++ ProCompartido11/src/main/java/com/mycompany/procompartido11/ProCompartido11.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.procompartido11;
+
+/**
+ *
+ * @author fernando_chang
+ */
+public class ProCompartido11 {
+
+    
+    public static void main(String[] args) {
+        int numeros=43;
+    String nombre="Fernando";
+        
+        System.out.println(numeros+nombre);
+    }
+}
